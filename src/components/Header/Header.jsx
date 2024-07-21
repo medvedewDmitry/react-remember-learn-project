d7301bc3ffe4a963033758e55af371ab8ad7cc9d
d.medvedev <medvedewdmitrij@yandex.ru>
2024-07-21 22:59:02 +0700

    - Добавил очистку счетчика

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 import logo from '/src/assets/logo-name.svg'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import classes from "./Header.module.css";
 export default function Header() {
     const [nowTime, setNowTime] = useState(new Date())
-    setInterval(() => {
-        setNowTime(new Date())
-    }, 1000)
+    useEffect(()=> {
+        const intervalTime = setInterval(() => {
+            setNowTime(new Date())
+        }, 1000)
+
+        return () => {
+            // Очистка переменных при уничтожении компонента
+            clearInterval(intervalTime);
+        }
+    }, []) // Если список зависимостей пустой, то эффект вызовется только при инициализации, так как не от чего не зависит
+
     const altName = 'Логотип'
     return (
         <header className={classes.header}>
             <img src={logo} alt={altName}/>
-            {/*<h3>Заголовок шапки</h3>*/}
             <span>Время сейчас: {nowTime.toLocaleTimeString('ru')}</span>
         </header>
     )
