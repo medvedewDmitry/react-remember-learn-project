fa15e1ccbc543fbedaaaefbc7fe0a2f3cb2aba66
d.medvedev <medvedewdmitrij@yandex.ru>
2024-07-21 23:16:05 +0700

    - Реализовал запрос и вывод информации с сервера

--- src/components/EffectsSection.jsx
+++ src/components/EffectsSection.jsx
@@ -1,10 +1,24 @@
 import Button from "./Button/Button.jsx";
 import Modal from "./Modal/Modal.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import WayToTeach from "./WayToTeach.jsx";
 
 export default function EffectsSection() {
     const [isShowModal, setIsShowModal] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [posts, setPosts] = useState([])
 
+    async function fetchPosts() {
+        setIsLoading(true)
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await response.json()
+        setPosts(posts)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
     const toggleModal = () => {
         setIsShowModal(!isShowModal)
     }
@@ -22,7 +36,23 @@ export default function EffectsSection() {
                     <Button onClick={toggleModal}>Закрыть</Button>
                 </Modal>
 
-
+                <h3 style={{marginTop: 10}}>Список постов</h3>
+                {
+                    isLoading && <p>Идет загрузка...</p>
+                }
+                {
+                    !isLoading && posts && <ul>
+                        {posts.map((post) => {
+                            return (
+                                <WayToTeach
+                                    key={post.id}
+                                    title={post.title}
+                                    desc={post.body}
+                                />
+                            )
+                        })}
+                    </ul>
+                }
             </section>
         </>
     )
